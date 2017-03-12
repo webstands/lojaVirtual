6068eb4b387420d4d19a6633a2c67bf26f6f69cb
webstands <webstands@gmail.com>
2017-03-12 19:45:50 -0300

    Add project files.

---
+++ Quiron.LojaVirtual.Test/UnitTestQuiron.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Quiron.LojaVirtual.Test
+{
+    [TestClass]
+    public class UnitTestQuiron
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
