4e4b4ca10be537f8e46cff893d82b842a8329d06
webstands <webstands@gmail.com>
2017-03-14 00:26:10 -0300

    Criação DbContext e Controller Produtos

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+    public DbSet<Produto> Produtos { get; set; }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuider)
+      {
+          modelBuider.Conventions.Remove<PluralizingTableNameConvention>();
+          modelBuider.Entity<Produto>().ToTable("Produtos");
+      }
+
+    }
+
+}
---
+++ Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private ProdutosRepositorio _repositoriorio;
+
+        public ActionResult Index()
+        {
+            _repositoriorio = new ProdutosRepositorio();
+            var produtos = _repositoriorio.Produtos.Take(10);
+            return View(produtos);
+        }
+    }
+}
